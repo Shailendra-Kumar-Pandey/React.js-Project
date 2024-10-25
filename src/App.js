9f79fdb31b58ba6d044e69601f3efa2ac09b2cbe
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-10-25 20:31:05 +0530

    dark mode toggle button Add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm'
+// import TextForm from './components/TextForm'
 
 
 // let name = " Shailendra Kumar Pandey"
@@ -10,7 +11,8 @@ function App() {
     <>
       <Navbar title="TextUtils" about="About"/>
       <div className="container my-3" >
-      <TextForm heading = "Enter the text Analyze" />
+        <About/>
+      {/* <TextForm heading = "Enter the text Analyze" /> */}
       </div>
     </>  
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,24 +2,57 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
     const upCase=()=>{
-        console.log("text" + text);
+        // console.log("text" + text);
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const clearText=()=>{
+        let clear = "";
+        setText(clear);
+    }
+    // const itelicFont =()=>{
+    //     let itelic = <i>{text}</i>
+    //     setText(itelic);
+    // }
+    const lowCase=()=>{
+        let lowerCase = text.toLowerCase();
+        setText(lowerCase);
+    }
     const onchangeText=(e)=>{
-        console.log("setText");
+        // console.log("setText");
         setText(e.target.value)
     }
+    const toggleItalic = () => {
+        setIsItalic(prev => !prev);
+    };
     const [text, setText] = useState("Enter the text below");
+    const [isItalic, setIsItalic] = useState(false);
     // text ="new test";//wronge way to change the state
     // setText("new text");//Correct way to change the state
     return (
-    <div>
+    <>
+        <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
          <textarea className="form-control" value={text} onChange={onchangeText} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={upCase}>Convert to UperCase</button>
-    </div>
+        <button className="btn btn-dark mx-2" onClick={upCase}>Convert to UperCase</button>
+        <button className="btn btn-primary mx-2" onClick={lowCase}>Convert to LowerCase</button>
+        <button className="btn btn-dark mx-2" onClick={clearText}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={toggleItalic}>
+                    {isItalic ? "Remove Italic" : "Convert to Italic Font"}
+                </button>
+        {/* <button className="btn btn-primary mx-2" onClick={itelicFont}>Convert to Itailic Font</button> */}
+
+        </div>
+        <div className="container my-3">
+            <h1> Your text Summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p style={{ fontStyle: isItalic ? 'italic' : 'normal' }}>{text}</p>
+            {/* <p>{text}</p> */}
+        </div>
+    </>
   )
 }
