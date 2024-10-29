a3aad2729992e71f190e581b80ca164161db4851
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-10-30 03:37:17 +0530

    Updates

---
+++ precache-manifest.f613572ebf17541bd467fc5bfdfb0c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2cb023464094e7a6c2543a097daf65",
+    "url": "/React.js-Project/index.html"
+  },
+  {
+    "revision": "2b6b67541f7d5663de2c",
+    "url": "/React.js-Project/static/css/main.009d59bc.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/React.js-Project/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/React.js-Project/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "2b6b67541f7d5663de2c",
+    "url": "/React.js-Project/static/js/main.2dba12c3.chunk.js"
+  },
+  {
+    "revision": "79978ecaadb2c70cd34f",
+    "url": "/React.js-Project/static/js/runtime~main.9b6d21af.js"
+  }
+]);
